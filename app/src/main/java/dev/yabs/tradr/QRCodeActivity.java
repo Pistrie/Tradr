cdab8dc7f650e2011f03e762a719da20669ba922
Sylvester Roos <6153719-Pistrie@users.noreply.gitlab.com>
2022-08-15 07:51:14 +0000

    Generated QR code now viewed in a new window

---
+++ app/src/main/java/dev/yabs/tradr/QRCodeActivity.java
@@ -0,0 +1,250 @@
+/*
+ * Tradr
+ * Copyright (c) 2022-2022, Sylvester Roos <pistrie@duck.com>
+ *
+ * Tradr is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Tradr is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tradr.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dev.yabs.tradr;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import dev.yabs.tradr.databinding.ActivityQrcodeBinding;
+
+public class QRCodeActivity extends AppCompatActivity {
+
+    private final String TAG = getClass().getSimpleName();
+
+    ImageView qrOutput;
+    TextView tvPrice;
+
+    private ActivityQrcodeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityQrcodeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        qrOutput = findViewById(R.id.qr_output);
+        tvPrice = findViewById(R.id.price);
+
+        String epc = null;
+        String price = null;
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            epc = bundle.getString("epc");
+            price = bundle.getString("price");
+        }
+        // initialize multi format writer
+        MultiFormatWriter writer = new MultiFormatWriter();
+        // initialize bit matrix
+        try {
+            BitMatrix matrix = writer.encode(epc, BarcodeFormat.QR_CODE, 350, 350);
+            // initialize barcode encoder
+            BarcodeEncoder encoder = new BarcodeEncoder();
+            // initialize bitmap
+            Bitmap bitmap = encoder.createBitmap(matrix);
+            // set bitmap on image view
+            qrOutput.setImageBitmap(bitmap);
+            tvPrice.setText("€ " + price);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    /**
+//     * Whether or not the system UI should be auto-hidden after
+//     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+//     */
+//    private static final boolean AUTO_HIDE = true;
+//
+//    /**
+//     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+//     * user interaction before hiding the system UI.
+//     */
+//    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+//
+//    /**
+//     * Some older devices needs a small delay between UI widget updates
+//     * and a change of the status and navigation bar.
+//     */
+//    private static final int UI_ANIMATION_DELAY = 300;
+//    private final Handler mHideHandler = new Handler(Looper.myLooper());
+//    private View mContentView;
+//    private final Runnable mHidePart2Runnable = new Runnable() {
+//        @SuppressLint("InlinedApi")
+//        @Override
+//        public void run() {
+//            // Delayed removal of status and navigation bar
+//            if (Build.VERSION.SDK_INT >= 30) {
+//                mContentView.getWindowInsetsController().hide(
+//                        WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+//            } else {
+//                // Note that some of these constants are new as of API 16 (Jelly Bean)
+//                // and API 19 (KitKat). It is safe to use them, as they are inlined
+//                // at compile-time and do nothing on earlier devices.
+//                mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+//                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+//            }
+//        }
+//    };
+//    private View mControlsView;
+//    private final Runnable mShowPart2Runnable = new Runnable() {
+//        @Override
+//        public void run() {
+//            // Delayed display of UI elements
+//            ActionBar actionBar = getSupportActionBar();
+//            if (actionBar != null) {
+//                actionBar.show();
+//            }
+//            mControlsView.setVisibility(View.VISIBLE);
+//        }
+//    };
+//    private boolean mVisible;
+//    private final Runnable mHideRunnable = new Runnable() {
+//        @Override
+//        public void run() {
+//            hide();
+//        }
+//    };
+//    /**
+//     * Touch listener to use for in-layout UI controls to delay hiding the
+//     * system UI. This is to prevent the jarring behavior of controls going away
+//     * while interacting with activity UI.
+//     */
+//    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+//        @Override
+//        public boolean onTouch(View view, MotionEvent motionEvent) {
+//            switch (motionEvent.getAction()) {
+//                case MotionEvent.ACTION_DOWN:
+//                    if (AUTO_HIDE) {
+//                        delayedHide(AUTO_HIDE_DELAY_MILLIS);
+//                    }
+//                    break;
+//                case MotionEvent.ACTION_UP:
+//                    view.performClick();
+//                    break;
+//                default:
+//                    break;
+//            }
+//            return false;
+//        }
+//    };
+//    private ActivityQrcodeBinding binding;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        binding = ActivityQrcodeBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//
+//        mVisible = true;
+//        mControlsView = binding.fullscreenContentControls;
+//        mContentView = binding.fullscreenContent;
+//
+//        // Set up the user interaction to manually show or hide the system UI.
+//        mContentView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                toggle();
+//            }
+//        });
+//
+//        // Upon interacting with UI controls, delay any scheduled hide()
+//        // operations to prevent the jarring behavior of controls going away
+//        // while interacting with the UI.
+//        binding.dummyButton.setOnTouchListener(mDelayHideTouchListener);
+//    }
+//
+//    @Override
+//    protected void onPostCreate(Bundle savedInstanceState) {
+//        super.onPostCreate(savedInstanceState);
+//
+//        // Trigger the initial hide() shortly after the activity has been
+//        // created, to briefly hint to the user that UI controls
+//        // are available.
+//        delayedHide(100);
+//    }
+//
+//    private void toggle() {
+//        if (mVisible) {
+//            hide();
+//        } else {
+//            show();
+//        }
+//    }
+//
+//    private void hide() {
+//        // Hide UI first
+//        ActionBar actionBar = getSupportActionBar();
+//        if (actionBar != null) {
+//            actionBar.hide();
+//        }
+//        mControlsView.setVisibility(View.GONE);
+//        mVisible = false;
+//
+//        // Schedule a runnable to remove the status and navigation bar after a delay
+//        mHideHandler.removeCallbacks(mShowPart2Runnable);
+//        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+//    }
+//
+//    private void show() {
+//        // Show the system bar
+//        if (Build.VERSION.SDK_INT >= 30) {
+//            mContentView.getWindowInsetsController().show(
+//                    WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+//        } else {
+//            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+//        }
+//        mVisible = true;
+//
+//        // Schedule a runnable to display UI elements after a delay
+//        mHideHandler.removeCallbacks(mHidePart2Runnable);
+//        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
+//    }
+//
+//    /**
+//     * Schedules a call to hide() in delay milliseconds, canceling any
+//     * previously scheduled calls.
+//     */
+//    private void delayedHide(int delayMillis) {
+//        mHideHandler.removeCallbacks(mHideRunnable);
+//        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+//    }
+}
